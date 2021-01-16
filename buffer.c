5ca982f9778b932ba90481474b8de53f52fe4edb
pwndumb <fhenrique314.42@gmail.com>
2021-01-16 20:58:54 -0300

    Update buffer.c

--- buffer.c
+++ buffer.c
@@ -6,9 +6,7 @@ void main(){
 	char buffer[64];
 	printf("Simples programa na linguagem C para explicar um estouro de buffer\n");
 	printf("E como isso pode levar usado para modificar o fluxo normal do programa\n");
-	
-	// apessar do que todos falam, o problema nao é a funcao gets mas sim o programador 
-	// nao validar a entrada de dados, confiando plenamente no usuario.
+
 
 	printf("Entre com um nome:?");
 	gets(buffer);
