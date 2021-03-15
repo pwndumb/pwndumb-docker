80cee1e0fe8df1e3774b6cb81fe512b8c046efab
pwndumb <fhenrique314.42@gmail.com>
2021-03-15 18:15:21 -0300

    add recur chall-utctf 2021

---
+++ ctf-solve/utctf-2021/reverse/recur/decode.py
@@ -0,0 +1,41 @@
+#! /usr/bin/python3
+from ctypes import *
+
+# keep the previous calculate values
+recurrence_cache={}
+
+def recurrence(n):
+
+    # check if recurrence(n) already in cache
+    if n in recurrence_cache:
+        return recurrence_cache[n]
+    if n ==0:
+        value = 3
+    if n == 1:
+        value = 5
+    elif n > 1:
+        value  = recurrence(n-2) * 3 + recurrence(n -1) * 2
+    
+    # save the value in recurrence_cache before return function
+    recurrence_cache[n] = value
+    
+    return value       
+
+def main():
+    flag_encoded = [0x76,0x71,0xc5,0xa9,0xe2,0x22,0xd8,0xb5,0x73,0xf1,0x92,0x28,0xb2,0xbf,0x90,0x5a,0x76,0x77,0xfc,0xa6,0xb3,0x21,0x90,0xda,0x6f,0xb5,0xcf,0x38]
+    flag_decoded= []
+
+    # iterate over encoded flag
+    for i in range(len(flag_encoded)):
+
+        rax = recurrence(i*i)
+        ebx= c_int32(rax).value ^ flag_encoded[i]
+        bx = c_int8(ebx).value
+        flag_decoded.append(bx)
+    
+    flag=''.join(chr(i) for i in flag_decoded)
+    print(f"[+] flag: {flag}")
+
+if __name__=='__main__':
+    main()
+
